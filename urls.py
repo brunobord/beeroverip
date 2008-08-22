fc16a55b6bd6b6f0a995b9e2eea07e926dd14f43
Bruno Bord <bruno@lachose>
2008-08-23 00:15:47 +0200

    default doesn't work yet

--- urls.py
+++ urls.py
@@ -7,7 +7,6 @@ info_dict = {
 info_dict_detail = info_dict.update({
     'slug_field': 'slug',
     'template_object_name': 'beer',
-    'slug': 'default',
 })
 
 urlpatterns = patterns('django.views.generic',
