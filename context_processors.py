9e6a1e4eb58b9de4576f753310999d9a9724fa63
Bruno Bord <bruno@lachose>
2008-09-02 23:21:20 +0200

    Added a new context processor, to pass the media_url. and 404 page now displays an empty dirty glass of beer. close #260528

--- context_processors.py
+++ context_processors.py
@@ -1,7 +1,10 @@
 # context_processors
+from django.conf import settings
+
+def media_url(request):
+    return {'media_url': settings.MEDIA_URL}
 
 def analytics_id(request):
-    from django.conf import settings
     try:
         return {'ANALYTICS_ID': settings.ANALYTICS_ID}
     except:
