a5c84844fa69b17d07f62fcd0945978612772e2a
Bruno Bord <bruno@lachose>
2008-09-06 01:20:16 +0200

    Now fully compatible with Django 1.0

--- context_processors.py
+++ context_processors.py
@@ -1,9 +1,6 @@
 # context_processors
 from django.conf import settings
 
-def media_url(request):
-    return {'media_url': settings.MEDIA_URL}
-
 def analytics_id(request):
     try:
         return {'ANALYTICS_ID': settings.ANALYTICS_ID}
