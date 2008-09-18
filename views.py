f655ce07a826193cb57321802b77137be070237f
Bruno Bord <bruno@lachose>
2008-09-18 22:46:09 +0200

    404 didn't get the MEDIA_URL stuff. Ugly workaround, but it works now.

--- views.py
+++ views.py
@@ -1,17 +1,27 @@
+from django.http import Http404
+from django.conf import settings
+from django.views.generic.simple import direct_to_template
 from beers.models import Beer
-from django.views.generic.list_detail import object_detail
 
 def beer_detail(request, slug):
     if not slug:
         slug = "default"
-    queryset = Beer.objects.order_by('slug')
-    return object_detail(request,
-        queryset = queryset,
-        slug = slug,
-        slug_field = 'slug',
-        template_object_name = 'beer',
+    try:
+        beer = Beer.objects.get(slug=slug)
+    except:
+        raise Http404
+
+    return direct_to_template(request,
+        template='beers/beer_detail.html',
+        extra_context={'beer': beer},
     )
 
 def beer_random(request):
     random_beer = Beer.objects.order_by('?')[0]
     return beer_detail(request, random_beer.slug)
+
+def custom_404_view(request):
+    response = direct_to_template(request, '404.html',
+        extra_context={'MEDIA_URL': settings.MEDIA_URL})
+    response.status_code = 404
+    return response
