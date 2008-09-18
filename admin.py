92ebba7d031d8e70b27af2f51564b66999dc0b87
Bruno Bord <bruno@lachose>
2008-09-18 21:02:41 +0200

    Cleanup

--- admin.py
+++ admin.py
@@ -1,6 +1,5 @@
 from beers.models import Beer
 from django.contrib import admin
-#from django.utils.translation import ugettext_lazy as _
 
 class BeerOptions(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
