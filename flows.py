be40fddf72a6831b784bead0b1d3853de955de6b
Bruno Bord <bruno@lachose>
2008-10-31 22:42:12 +0100

    i18n for the RSS feed

--- flows.py
+++ flows.py
@@ -1,10 +1,11 @@
 from django.contrib.syndication.feeds import Feed
+from django.utils.translation import ugettext as _
 from models import Beer
 
 class BeerFlow(Feed):
-    title = "Beer flow, latest"
+    title = _("Beer flow, latest")
     link = "/all/"
-    description = "Latest Beers available for IP Transport"
+    description = _("Latest Beers available for IP Transport")
 
     def items(self):
         return Beer.objects.order_by('-upload_date')[:10]
