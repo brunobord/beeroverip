7501e4a8d24cb8b1f4015bbb23999c744b6417df
Bruno Bord <bruno@lachose>
2008-11-13 23:47:58 +0100

    Merging with the "not a beer" branch. It's a fresh new way to send a non-beer drink over IP.

--- tests.py
+++ tests.py
@@ -3,9 +3,9 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
 from django.conf import settings
-from models import Beer
+from models import Beer, NotABeer
 
-class TestUrls(TestCase):
+class TestBeerUrls(TestCase):
     """We just have to test URLs, by the way..."""
     fixtures = ['data']
 
@@ -67,7 +67,8 @@ class TestUrls(TestCase):
         response = self.client.get(reverse('beer_list'))
         self.assertEquals(response.status_code, 200)
 
-class TestImages(TestCase):
+
+class TestBeerImages(TestCase):
     """Testing image attributes"""
     fixtures = ['data']
 
@@ -79,4 +80,67 @@ class TestImages(TestCase):
         for beer in Beer.objects.all():
             self.assertTrue(max(beer.picture.height, beer.picture.width) <= 500)
 
-# Eof
+
+class TestNotABeerUrls(TestCase):
+    """We just have to test URLs, by the way..."""
+    fixtures = ['notabeer']
+
+    def test_000(self):
+        "not a test case, just for the user's information"
+        print
+        print "FYI:"
+        print "num. of drinks: %s" % NotABeer.objects.count()
+        print
+
+    def test_home(self):
+        """Home page.
+        Should return 'something' (this means we have a default drink)"""
+        response = self.client.get('/notabeer/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_404(self):
+        """404 exception test."""
+        response = self.client.get('/dehfzihfezihgzihgz/')
+        self.assertEquals(response.status_code, 404)
+        self.assertContains(response, settings.MEDIA_URL, status_code=404)
+
+    def test_media_files(self):
+        """Every drink has a picture. This picture should exist."""
+        for drink in NotABeer.objects.all():
+            self.assertNotEquals(drink.picture, "")
+            response = self.client.get(drink.picture.url)
+            self.assertEquals(response.status_code, 200)
+
+    def test_media_file_not_found(self):
+        """If any drink picture file is unknown/missing, it must be an error."""
+        random_drink = NotABeer.objects.get(pk=1)
+        random_drink.picture = 'unknown/file.jpg'
+        random_drink.save()
+        response = self.client.get(random_drink.picture.url)
+        self.assertNotEquals(response.status_code, 200)
+
+    def test_data(self):
+        """Every drink has to be tested individually."""
+        for drink in NotABeer.objects.all():
+            response = self.client.get(drink.get_absolute_url())
+            if response.status_code != 200:
+                # For debug purposes.
+                print drink
+            self.assertEquals(response.status_code, 200)
+
+    def test_random(self):
+        """The random view should return a drink view"""
+        response = self.client.get(reverse('drink_random'))
+        self.assertEquals(response.status_code, 200)
+
+##    def test_rss_flow(self):
+##        """Testing the RSS flow"""
+##        response = self.client.get('/flow/drinks/')
+##        self.assertEquals(response.status_code, 200)
+
+    def test_url_all(self):
+        """Testing '/all/' url"""
+        response = self.client.get(reverse('drink_list'))
+        self.assertEquals(response.status_code, 200)
+
+# EOF
