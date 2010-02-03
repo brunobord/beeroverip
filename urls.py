cbd0c5f23d4eaf972247f02040399781a0ea11c0
Bruno Bord <bruno@jehaisleprintemps.net>
2010-02-03 12:43:56 +0100

    some people say it's faster

--- urls.py
+++ urls.py
@@ -9,16 +9,20 @@ flows = {
 }
 
 urlpatterns = patterns('',
+    url(r'^flow/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': flows}),
+)
+
+urlpatterns += patterns('beers.views',
+
     # not a beer
-    url(r'^notabeer/all/', drink_list, name='drink_list'),
-    url(r'^notabeer/random/', drink_random, name='drink_random'),
-    url(r'^notabeer/(?P<slug>[\w-]+)?/?', drink_detail),
-    url(r'^notabeer/(?P<slug>[\w-]+)?', drink_detail, name="drink_detail"),
+    url(r'^notabeer/all/', 'drink_list', name='drink_list'),
+    url(r'^notabeer/random/', 'drink_random', name='drink_random'),
+    url(r'^notabeer/(?P<slug>[\w-]+)?/?', 'drink_detail'),
+    url(r'^notabeer/(?P<slug>[\w-]+)?', 'drink_detail', name="drink_detail"),
 
     # beers
-    url(r'^all/', beer_list, name="beer_list"),
-    url(r'^random/', beer_random, name="beer_random"),
-    url(r'^flow/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': flows}),
-    url(r'^(?P<slug>[\w-]+)?/?', beer_detail),
-    url(r'^(?P<slug>[\w-]+)?/', beer_detail, name="beer_detail"),
+    url(r'^all/', 'beer_list', name="beer_list"),
+    url(r'^random/', 'beer_random', name="beer_random"),
+    url(r'^(?P<slug>[\w-]+)?/?', 'beer_detail'),
+    url(r'^(?P<slug>[\w-]+)?/', 'beer_detail', name="beer_detail"),
 )
